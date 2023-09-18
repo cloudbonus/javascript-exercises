b0843d5460f432d4013c1f9d5f4bd1379a589ff6
cloudbonus <devFlynn@proton.me>
2023-09-18 14:39:45 +0300

    solve 8-12 tasks

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  if (array.length == 0) return 0
+  return array.reduce((acc, curr) => acc + curr)
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  if (array.length == 0) return 0
+  return array.reduce((acc, curr) => acc * curr)
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  return x ** y
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  if (x == 0) return 1
+  else return x * factorial(x - 1)
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    res = [...string.toLowerCase()].filter(c => c.match('[a-z0-9]'))
+    return res.join('') == res.reverse().join('')
 };
 
 // Do not edit below this line
